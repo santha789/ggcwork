f470e172e80235e3dc556481586c69cb090f5abc
GGC Work <ggcwork@ggclinkgroup.com>
2026-08-11 17:51:33 +0700

    fix(app): pakai tools:node=remove agar manifest merger buang ACTIVITY_RECOGNITION dari library

--- plugins/removeUnusedPermissions.js
+++ plugins/removeUnusedPermissions.js
@@ -5,11 +5,25 @@ const PERMISSIONS_TO_REMOVE = ['android.permission.ACTIVITY_RECOGNITION'];
 module.exports = function removeUnusedPermissions(config) {
   return withAndroidManifest(config, (config) => {
     const manifest = config.modResults;
-    const usesPermissions = manifest.manifest['uses-permission'];
-    if (usesPermissions && Array.isArray(usesPermissions)) {
-      config.modResults.manifest['uses-permission'] = usesPermissions.filter(
-        (perm) => !PERMISSIONS_TO_REMOVE.includes(perm.$ && perm.$['android:name'])
-      );
+    if (!manifest.manifest) return config;
+
+    const usesPermission = manifest.manifest['uses-permission'];
+    const perms = Array.isArray(usesPermission) ? usesPermission : [];
+
+    manifest.manifest['uses-permission'] = perms.filter(
+      (perm) => !PERMISSIONS_TO_REMOVE.includes(perm.$ && perm.$['android:name'])
+    );
+
+    manifest.manifest.$ = manifest.manifest.$ || {};
+    manifest.manifest.$['xmlns:tools'] = 'http://schemas.android.com/tools';
+
+    for (const name of PERMISSIONS_TO_REMOVE) {
+      manifest.manifest['uses-permission'].push({
+        $: {
+          'android:name': name,
+          'tools:node': 'remove',
+        },
+      });
     }
     return config;
   });
