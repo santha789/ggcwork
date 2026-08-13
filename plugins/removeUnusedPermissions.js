9b5e5451738489d58d843212976e8a7826bea712
GGC Work <ggcwork@ggclinkgroup.com>
2026-08-13 11:52:24 +0700

    chore(app): aktifkan R8/proguard + shrink resources, override orientasi ML Kit activity (pembersihan warning Play Console)

--- plugins/removeUnusedPermissions.js
+++ plugins/removeUnusedPermissions.js
@@ -2,6 +2,10 @@ const { withAndroidManifest } = require('@expo/config-plugins');
 
 const PERMISSIONS_TO_REMOVE = ['android.permission.ACTIVITY_RECOGNITION'];
 
+const ACTIVITIES_ORIENTATION_UNSPECIFIED = [
+  'com.google.mlkit.vision.codescanner.internal.GmsBarcodeScanningDelegateActivity',
+];
+
 module.exports = function removeUnusedPermissions(config) {
   return withAndroidManifest(config, (config) => {
     const manifest = config.modResults;
@@ -25,6 +29,29 @@ module.exports = function removeUnusedPermissions(config) {
         },
       });
     }
+
+    const application = manifest.manifest['application'];
+    if (application && Array.isArray(application)) {
+      const activities = application[0] && application[0]['activity'];
+      if (activities && Array.isArray(activities)) {
+        for (const activity of activities) {
+          const name = activity.$ && activity.$['android:name'];
+          if (ACTIVITIES_ORIENTATION_UNSPECIFIED.includes(name)) {
+            activity.$['android:screenOrientation'] = 'unspecified';
+          }
+        }
+        for (const name of ACTIVITIES_ORIENTATION_UNSPECIFIED) {
+          application[0]['activity'].push({
+            $: {
+              'android:name': name,
+              'android:screenOrientation': 'unspecified',
+              'tools:replace': 'android:screenOrientation',
+            },
+          });
+        }
+      }
+    }
+
     return config;
   });
 };
