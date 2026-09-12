a64e0a4bc45dacdb8e64894e43d70ce984a84f21
GGC Work <ggcwork@ggclinkgroup.com>
2026-09-12 20:59:55 +0700

    chore(version): bump ke 1.0.22

--- src/theme.js
+++ src/theme.js
@@ -24,7 +24,7 @@ export const APP_VERSION =
   Constants.expoConfig?.version ||
   Constants.manifest2?.extra?.expoClient?.version ||
   Application.nativeApplicationVersion ||
-  '1.0.21';
+  '1.0.22';
 
 export function fmtMoney(v) {
   return fmtRp(v);
